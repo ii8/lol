46821a596064ac2f185d63b925bee977c31cbea2
Stewart <hello@stewartwebb.co.uk>
2015-10-29 00:09:40 +0000

    Stewarts menu page. WIP

--- Handler/Menu.hs
+++ Handler/Menu.hs
@@ -1,6 +1,19 @@
 module Handler.Menu where
 
 import Import
+import Data.Ord (comparing)
 
 getMenuR :: Handler Html
-getMenuR = defaultLayout $(widgetFile "menu")
+getMenuR = do
+  let rows = query
+
+  defaultLayout $ do
+    $(widgetFile "menu")
+
+query = do
+  cats <- selectList [] [Asc CategoryName]
+  forM cats $ \(Entity catId cat) -> do
+      products <- selectList
+          [ProductCategory ==. catId]
+          [Asc ProductName]
+      return (cat, map entityVal products)
