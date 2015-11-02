5e6930555230cc7d257f6cd4e3bb1023bd3d7bd5
Stewart <hello@stewartwebb.co.uk>
2015-11-02 03:06:07 +0000

    Whoops. Pls no flamerino. Command line git is 2 hard 4 me.

--- Handler/Menu.hs
+++ Handler/Menu.hs
@@ -1,11 +1,7 @@
 module Handler.Menu where
 
 import Import
-<<<<<<< HEAD
-=======
-import Data.Ord (comparing)
 import Database.Persist.Sql (fromSqlKey)
->>>>>>> Some more menu changes and handler change thingy that makes the product id thingy work.
 
 getMenuR :: Handler Html
 getMenuR = do
