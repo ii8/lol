41eca1bce302bfe28f1a42633cf8c06b6b248b05
Murray <murray.calavera@gmail.com>
2015-12-24 17:58:02 +0000

    all round me burdens seem to fall

--- test/TestImport.hs
+++ test/TestImport.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoCPP #-}
+
 module TestImport
     ( module TestImport
     , module X
@@ -24,7 +26,7 @@ runDBWithApp app query = runSqlPersistMPool query (appConnPool app)
 withApp :: SpecWith App -> Spec
 withApp = before $ do
     settings <- loadAppSettings
-        ["config/test-settings.yml", "config/settings.yml"]
+        ["config/test-settings.yml"]
         []
         ignoreEnv
     foundation <- makeFoundation settings
@@ -46,6 +48,24 @@ wipeDB app = runDBWithApp app $ do
     rawExecute "SET foreign_key_checks = 0;" []
     forM_ queries (\q -> rawExecute q [])
     rawExecute "SET foreign_key_checks = 1;" []
+
+    rawExecute "insert into `deployment` set \
+               \`name` = 'test',\
+               \`domain` = 'localhost',\
+               \`wrapper` = 'navbar',\
+               \`stripe_public` = 'pk_test_wUAzZy675JJKqVCFMU2FcSBB',\
+               \`stripe_secret` = 'sk_test_cC82zVtq4YT0Y90ZuXesESTX';" []
+    rawExecute "insert into `piece` set \
+               \`template` = 'test';" []
+    rawExecute "insert into `page` set \
+               \`deployment` = (select `id` from `deployment` where `domain` = 'localhost'),\
+               \`name` = 'home',\
+               \`piece` = 1;" []
+    rawExecute "insert into `piece_data` set \
+               \`piece` = 1,\
+               \`key` = 'mainbox',\
+               \`value` = 'Hello I am custom page data',\
+               \`type` = 'Plain';" []
 
 getTables :: MonadIO m => ReaderT SqlBackend m [Text]
 getTables = do
